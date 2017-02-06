2ebfc88afca7796042c3e23e1639eee615260bf1
Crosseye Jack <razo.rocks@gmail.com>
2017-02-06 16:57:49 +0000

    git init

---
+++ plugin.rb
@@ -0,0 +1,9 @@
+# name: Twitch Chat Thingy
+# about: Embedds Twitch Chat onto the site
+# version: dev
+# authors: Crosseye Jack
+# url: https://github.com/CrosseyeJack/discourse-twitch-chat
+
+register_asset "stylesheets/chat.scss"
+
+enabled_site_setting :twitchchat_enabled
