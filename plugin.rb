71627ced5b8708103bd7cd808a55bb82c19aeb8f
Crosseye Jack <razo.rocks@gmail.com>
2017-02-14 23:12:26 +0000

    Why am i init'ing the settings dependant on if the settings are enabled? Why cause lack of documentation thats why...

--- plugin.rb
+++ plugin.rb
@@ -11,15 +11,13 @@ DiscoursePluginRegistry.serialized_current_user_fields << "enable_twitch_chat"
 
 after_initialize do
 	User.register_custom_field_type('enable_twitch_chat', :boolean)
-	if SiteSetting.show_twitch_chat then
-    add_to_serializer(:user, :custom_fields, false) {
-      if object.custom_fields == nil then
-        {}
-      else
-        object.custom_fields
-      end
-    }
-  end
+	add_to_serializer(:user, :custom_fields, false) {
+		if object.custom_fields == nil then
+			{}
+		else
+			object.custom_fields
+		end
+	}
 end
 
 register_asset "stylesheets/chat.scss"
