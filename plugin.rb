27e7f97595b463761e0138ab67778bea3bb7602b
Crosseye Jack <razo.rocks@gmail.com>
2017-02-14 15:26:21 +0000

    forgot a file

--- plugin.rb
+++ plugin.rb
@@ -4,9 +4,22 @@
 # authors: Crosseye Jack
 # url: https://github.com/CrosseyeJack/discourse-twitch-chat
 
-register_asset "stylesheets/chat.scss"
+
+enabled_site_setting :show_twitch_chat
+
+DiscoursePluginRegistry.serialized_current_user_fields << "enable_twitch_chat"
 
 after_initialize do
-    module ::TwitchChat
-    end
-	end
+	User.register_custom_field_type('enable_twitch_chat', :boolean)
+	if SiteSetting.show_twitch_chat then
+    add_to_serializer(:user, :custom_fields, false) {
+      if object.custom_fields == nil then
+        {}
+      else
+        object.custom_fields
+      end
+    }
+  end
+end
+
+register_asset "stylesheets/chat.scss"
